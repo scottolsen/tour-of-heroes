416f075aa310581fc101a0b46e55d7d8fb8d0795
Scott Olsen <solsen300@gmail.com>
2016-03-29 13:28:28 -0500

    services

---
+++ app/hero.service.ts
@@ -0,0 +1,15 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly() {
+     return new Promise<Hero[]>(resolve =>
+       setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+     );
+   }
+}
